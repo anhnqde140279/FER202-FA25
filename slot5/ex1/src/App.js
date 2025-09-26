b17387dd5ed0c138c126a150399b0d1e92e1eb7a
QuynhAnh4100 <mka251099@gmail.com>
2025-09-26 10:25:59 +0700

    Cập nhật code mới

---
+++ slot5/ex1/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p1>Hello <strong>Exercise 1</strong></p1>
+    </div>
+  );
+}
+
+export default App;
---
+++ slot5/ex1/src/components/exercise1.jsx
@@ -0,0 +1,11 @@
+export function Exercise1() {
+    const double = x => x*2;
+    const issPositive = x => x > 0;
+    
+  return (
+    <div className="Exercise1">
+      <p>Hello <strong>Exercise 1</strong></p>
+      <h2>Chi tiết bào tập 1</h2>
+    </div>
+  );
+}
